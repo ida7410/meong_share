f9525c950bf40d2383d7491d514b38f36f810452
ida741 <giruida@gmail.com>
2025-07-19 07:23:02 -0400

    Updated for deploying jsp

--- src/main/java/com/ms/MeongShareApplication.java
+++ src/main/java/com/ms/MeongShareApplication.java
@@ -2,11 +2,19 @@ package com.ms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling // 스케줄러
 @SpringBootApplication
-public class MeongShareApplication {
+public class MeongShareApplication extends SpringBootServletInitializer {
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(MeongShareApplication.class);
+    }
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(MeongShareApplication.class, args);
